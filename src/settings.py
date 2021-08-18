7d0403b980e9b69365bd7bc095bacab50be5bd16
Your Name <c5sc3.gri.n4w7@gamil.com>
2021-08-18 23:27:24 +0900

    🐞fix: BASE_DIR in settings

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,5 @@
 import os
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'data', 'preprocessing', 'template.jpg')
